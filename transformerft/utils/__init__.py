38b79eb9e7e44f98f5eba79b83f640a1365450a9
Anton Lu <anton.lu@cern.ch>
2023-07-31 19:23:27 +0200

    add signal processing module

--- transformerft/utils/__init__.py
+++ transformerft/utils/__init__.py
@@ -1,5 +1,7 @@
 from . import _ops as ops
+from . import _signal as signal
 
-ops.__module__ = __name__ + 'ops'
+ops.__module__ = __name__ + "ops"
+signal.__module__ = __name__ + "signal"
 
-__all__ = ["ops"]
+__all__ = ["ops", "signal"]
