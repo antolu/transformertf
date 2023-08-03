17867d054d0bac8009e3d926e2af3642c349a02c
Anton Lu <anton.lu@cern.ch>
2023-08-03 16:07:50 +0200

    Add PhyLSTMDataModule to __init__.py

--- transformerft/models/phylstm/__init__.py
+++ transformerft/models/phylstm/__init__.py
@@ -1,5 +1,6 @@
 from ..._mod_replace import replace_modname
 from ._config import PhyLSTMConfig
+from ._datamodule import PhyLSTMDataModule
 from ._lightning import STEP_OUTPUT, PhyLSTMModule
 from ._loss import LossWeights, PhyLSTMLoss
 from ._model import PhyLSTM1, PhyLSTM2, PhyLSTM3
@@ -13,6 +14,7 @@ for _mod in (
     PhyLSTM2,
     PhyLSTM3,
     PhyLSTMModule,
+    PhyLSTMDataModule,
 ):
     replace_modname(_mod, __name__)
 
@@ -28,6 +30,7 @@ __all__ = [
     "PhyLSTM2",
     "PhyLSTM3",
     "PhyLSTMModule",
+    "PhyLSTMDataModule",
     "STEP_OUTPUT",
     "PhyLSTM1Output",
     "PhyLSTM2Output",
