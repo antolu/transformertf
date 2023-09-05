6740fbe1dd14426f3ea58f2a43d22645f15bbe4f
Anton Lu <anton96lu@gmail.com>
2023-09-05 16:31:36 +0200

    fix tests

--- tests/integration/data/test_data_transform_inverse_transform.py
+++ tests/integration/data/test_data_transform_inverse_transform.py
@@ -2,12 +2,14 @@
 from __future__ import annotations
 
 import numpy as np
-import pytest
 import pandas as pd
+import pytest
+
 from transformertf.data import DataModuleBase
 from transformertf.models.phylstm import PhyLSTMDataModule
 from transformertf.utils import ops
-from ...conftest import DF_PATH, CURRENT, FIELD
+
+from ...conftest import CURRENT, DF_PATH, FIELD
 
 
 @pytest.fixture(scope="module")
@@ -58,7 +60,6 @@ def test_data_transform_inverse_transform(dm: DataModuleBase) -> None:
     assert np.allclose(y, y_true)
 
 
-
 @pytest.fixture(scope="module")
 def physical_dm() -> PhyLSTMDataModule:
     dm = PhyLSTMDataModule(
@@ -71,12 +72,13 @@ def physical_dm() -> PhyLSTMDataModule:
     return dm
 
 
-def test_physical_data_transform_inverse_transform(physical_dm: PhyLSTMDataModule) -> None:
+def test_physical_data_transform_inverse_transform(
+    physical_dm: PhyLSTMDataModule,
+) -> None:
     df = pd.read_parquet(DF_PATH)
     df = df.dropna()
     df = df.reset_index(drop=True)
     df = df[[CURRENT, FIELD]]
-    df[f"{FIELD}_dot"] = np.gradient(df[FIELD].values)
 
     dataset = physical_dm.val_dataset
 
@@ -100,7 +102,7 @@ def test_physical_data_transform_inverse_transform(physical_dm: PhyLSTMDataModul
     y = y.numpy()
 
     x_true = df[CURRENT].values
-    y_true = df[[FIELD, f"{FIELD}_dot"]].values
+    y_true = df[FIELD].values
 
     assert np.allclose(x, x_true)
     assert np.allclose(y, y_true)
