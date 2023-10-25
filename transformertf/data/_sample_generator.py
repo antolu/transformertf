7a83a06fea46e8e4a4deb7c19576698e6ac744b3
Anton Lu <anton.lu@cern.ch>
2023-10-25 21:32:47 +0200

    correct input for transformer sample generator

--- transformertf/data/_sample_generator.py
+++ transformertf/data/_sample_generator.py
@@ -222,7 +222,10 @@ class TransformerSampleGenerator(SampleGenerator[TransformerSample[T]]):
         tgt_slice = slice(sl.start + self._src_seq_len, sl.stop)
 
         src = stack(self._input_data[src_slice], self._label_data[src_slice])
-        tgt = stack(self._input_data[tgt_slice], zeros_like(self._input_data[tgt_slice]))
+        tgt = stack(
+            self._input_data[tgt_slice],
+            zeros_like(self._input_data[tgt_slice]),
+        )
         label = self._label_data[tgt_slice]
 
         if label.ndim == 1:
