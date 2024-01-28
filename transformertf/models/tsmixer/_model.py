b5e4d5e4dc28124fd62662fb6b95ba7701515509
Anton Lu <anton.lu@cern.ch>
2024-01-28 14:51:28 +0100

    use mlp for tsmixer final step

--- transformertf/models/tsmixer/_model.py
+++ transformertf/models/tsmixer/_model.py
@@ -147,11 +147,13 @@ class TSMixer(torch.nn.Module):
 
         if out_dim is not None:
             # use MLP for final prediction
-            self.fc = torch.nn.Sequential([
-                torch.nn.Linear(hidden_dim, fc_dim),
-                torch.nn.ReLU(),
-                torch.nn.Linear(fc_dim, out_dim),
-            ])
+            self.fc = torch.nn.Sequential(
+                [
+                    torch.nn.Linear(hidden_dim, fc_dim),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(fc_dim, out_dim),
+                ]
+            )
         else:
             self.fc = None
 
