137ae7b6b194874c1cb848db15ddca4b6215f3f8
Anton Lu <anton96lu@gmail.com>
2024-02-08 12:43:28 +0100

    refactor typing out from lightning base module

--- transformertf/models/_base_module.py
+++ transformertf/models/_base_module.py
@@ -10,24 +10,16 @@ from ..config import BaseConfig
 from ..data import TimeSeriesSample
 from ..utils import configure_lr_scheduler, configure_optimizers, ops
 
-MODEL_INPUT = typing.Union[torch.Tensor, dict[str, torch.Tensor]]
-MODEL_OUTPUT = typing.Union[torch.Tensor, dict[str, torch.Tensor]]
-MODEL_STATES = typing.Union[torch.Tensor, dict[str, torch.Tensor]]
-
-STEP_OUTPUT = typing.Union[MODEL_OUTPUT, dict[str, MODEL_OUTPUT]]
-EPOCH_OUTPUT = list[STEP_OUTPUT]
-
-OPT_CALL_TYPE = typing.Callable[
-    [tuple[typing.Any, ...]], torch.optim.Optimizer
-]
-LR_CALL_TYPE = typing.Callable[
-    [tuple[typing.Any, ...]], torch.optim.lr_scheduler.LRScheduler
-]
-
 
 if typing.TYPE_CHECKING:
     SameType = typing.TypeVar("SameType", bound="LightningModuleBase")
     from ..utils import OPTIMIZER_DICT
+    from .typing import (
+        LR_CALL_TYPE,
+        MODEL_OUTPUT,
+        OPT_CALL_TYPE,
+        STEP_OUTPUT,
+    )
 
 
 class LightningModuleBase(L.LightningModule):
