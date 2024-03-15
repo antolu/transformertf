b93ec7fe8a18e11ada4a45f739e50a99cad0604d
Anton Lu <anton96lu@gmail.com>
2024-03-15 11:52:57 +0100

    add AddNorm layer

--- transformertf/nn/__init__.py
+++ transformertf/nn/__init__.py
@@ -1,10 +1,12 @@
 from .._mod_replace import replace_modname
+from ._add_norm import AddNorm
 from ._glu import GatedLinearUnit
 from ._grn import GatedResidualNetwork
 from ._mlp import MLP
 from ._quantile_loss import QuantileLoss
 
 for _mod in (
+    AddNorm,
     GatedLinearUnit,
     GatedResidualNetwork,
     QuantileLoss,
@@ -16,6 +18,7 @@ del _mod
 del replace_modname
 
 __all__ = [
+    "AddNorm",
     "GatedLinearUnit",
     "GatedResidualNetwork",
     "QuantileLoss",
