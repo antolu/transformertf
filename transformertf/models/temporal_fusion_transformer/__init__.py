a430a647686542a79e9481ddbceacf4bc5ea9c24
Anton Lu <anton.lu@cern.ch>
2024-03-16 23:39:09 +0100

    add TFTConfig and TemporalFusionConfig

--- transformertf/models/temporal_fusion_transformer/__init__.py
+++ transformertf/models/temporal_fusion_transformer/__init__.py
@@ -1,12 +1,19 @@
 from ._model import TemporalFusionTransformer  # noqa: F401
+from ._config import TemporalFusionTransformerConfig  # noqa: F401
 
 from ..._mod_replace import replace_modname
 
 
-for _mod in (TemporalFusionTransformer,):
+for _mod in (TemporalFusionTransformer, TemporalFusionTransformer):
     replace_modname(_mod, __name__)
 
 del replace_modname
 del _mod
 
-__all__ = ["TemporalFusionTransformer"]
+TFTConfig = TemporalFusionTransformerConfig
+
+__all__ = [
+    "TemporalFusionTransformer",
+    "TemporalFusionTransformerConfig",
+    "TFTConfig",
+]
