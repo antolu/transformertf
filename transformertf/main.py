605e235ec2b3e2e221cd47b78b7bbf08fe41b705
Anton Lu <anton.lu@cern.ch>
2024-05-25 22:20:40 +0200

    fix custom LightningCLI default arguments and loss weight init

--- transformertf/main.py
+++ transformertf/main.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
+import os
 import typing
 
+import jsonargparse
 import lightning.pytorch.cli
 import pytorch_optimizer  # noqa: F401
 import torch
 from lightning.pytorch.cli import LightningArgumentParser
 
 from transformertf.data import (
+    DataModuleBase,
     EncoderDataModule,  # noqa: F401
     EncoderDecoderDataModule,  # noqa: F401
     TimeSeriesDataModule,  # noqa: F401
 )
+from transformertf.models import LightningModuleBase
 from transformertf.models.lstm import LSTM  # noqa: F401
 from transformertf.models.phylstm import (  # noqa: F401
     PhyLSTM,
@@ -36,63 +40,63 @@ class LightningCLI(lightning.pytorch.cli.LightningCLI):
         parser.add_lightning_class_args(
             lightning.pytorch.callbacks.LearningRateMonitor, "lr_monitor"
         )
-        parser.set_defaults({"lr_monitor": {"logging_interval": "epoch"}})
+        parser.set_defaults({"lr_monitor.logging_interval": "epoch"})
 
         parser.add_lightning_class_args(
             lightning.pytorch.callbacks.RichProgressBar, "progress_bar"
         )
-        parser.set_defaults({"progress_bar": {"refresh_rate": 1}})
+        parser.set_defaults({"progress_bar.refresh_rate": 1})
 
         parser.add_lightning_class_args(
             lightning.pytorch.callbacks.RichModelSummary, "model_summary"
         )
-        parser.set_defaults({"model_summary": {"max_depth": 2}})
+        parser.set_defaults({"model_summary.max_depth": 2})
 
         parser.add_lightning_class_args(
             lightning.pytorch.callbacks.ModelCheckpoint, "checkpoint_every"
         )
         parser.set_defaults({
-            "checkpoint_every": {
-                "save_top_k": -1,
-                "monitor": "loss/validation",
-                "mode": "min",
-                "every_n_epochs": 1,
-                "filename": "epoch={epoch}-every-valloss={loss/validation:.4f}",
-            }
+            "checkpoint_every.save_top_k": -1,
+            "checkpoint_every.monitor": "loss/validation",
+            "checkpoint_every.mode": "min",
+            "checkpoint_every.dirpath": "checkpoints",
+            "checkpoint_every.filename": "epoch={epoch}-valloss={loss/validation:.4f}",
+            "checkpoint_every.every_n_epochs": 50,
         })
 
         parser.add_lightning_class_args(
             lightning.pytorch.callbacks.ModelCheckpoint, "checkpoint_best"
         )
         parser.set_defaults({
-            "checkpoint_best": {
-                "save_top_k": 1,
-                "monitor": "loss/validation",
-                "mode": "min",
-                "dirpath": "checkpoints",
-                "filename": "epoch={epoch}-valloss={loss/validation:.4f}",
-                "save_last": "link",
-                "save_weights_only": False,
-                "auto_insert_metric_name": False,
-                "enable_version_counter": False,
-            }
+            "checkpoint_best.save_top_k": 1,
+            "checkpoint_best.monitor": "loss/validation",
+            "checkpoint_best.mode": "min",
+            "checkpoint_best.dirpath": "checkpoints",
+            "checkpoint_best.filename": "epoch={epoch}-valloss={loss/validation:.4f}",
+            "checkpoint_best.save_last": "link",
+            "checkpoint_best.save_weights_only": False,
+            "checkpoint_best.auto_insert_metric_name": False,
+            "checkpoint_best.enable_version_counter": False,
         })
 
         parser.set_defaults({
-            "trainer": {
-                "logger": {
-                    "class_path": "lightning.pytorch.loggers.TensorBoardLogger",
-                    "init_args": {
-                        "log_graph": True,
-                        "save_dir": "logs",
-                        "name": None,
-                    },
-                }
-            }
+            "trainer.logger": jsonargparse.lazy_instance(
+                lightning.pytorch.loggers.TensorBoardLogger,
+                save_dir="logs",
+                name=None,
+            ),
         })
 
     def before_fit(self) -> None:
         self.model = self._maybe_compile(self.model)
+
+        # hijack model checkpoint callbacks to save to checkpoint_dir/version_{version}
+        version = self.trainer.logger.version
+        version_str = f"version_{version}"
+
+        for callback in self.trainer.callbacks:
+            if isinstance(callback, lightning.pytorch.callbacks.ModelCheckpoint):
+                callback.dirpath = os.path.join(callback.dirpath, version_str)
 
     def before_validate(self) -> None:
         self.model = self._maybe_compile(self.model)
@@ -113,4 +117,6 @@ class LightningCLI(lightning.pytorch.cli.LightningCLI):
 
 
 def main() -> None:
-    LightningCLI()
+    LightningCLI(
+        subclass_mode_model=LightningModuleBase, subclass_mode_data=DataModuleBase
+    )
