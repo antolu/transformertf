3c1f890353dda70890b5a89ecd58bd82fc09d23e
Anton Lu <anton.lu@cern.ch>
2024-05-29 17:14:15 +0200

    remove architectural parameters from the pbt tuning scripts

--- tune_example/tune_pbt.py
+++ tune_example/tune_pbt.py
@@ -30,34 +30,53 @@ CONFIG_PATH = HERE / "lstm_config.yml"
 
 
 GRID = {
-    "num_layers": ray.tune.choice([1, 2, 3]),
-    "hidden_dim": ray.tune.choice([100, 200, 300, 400, 500]),
-    "hidden_dim_fc": ray.tune.choice([512, 1024, 2048]),
     "dropout": ray.tune.uniform(0.1, 0.5),
     "lr": ray.tune.loguniform(1e-5, 1e-2),
     "batch_size": ray.tune.choice([32, 64, 128, 256, 512]),
-    "seq_len": ray.tune.choice([300, 600, 900, 1200]),
 }
 
 PARAM2KEY = {
-    "num_layers": "model.init_args.num_layers",
-    "hidden_dim": "model.init_args.hidden_dim",
-    "hidden_dim_fc": "model.init_args.hidden_dim_fc",
     "dropout": "model.init_args.dropout",
     "lr": "optimizer.init_args.lr",
     "batch_size": "data.init_args.batch_size",
-    "seq_len": "data.init_args.seq_len",
 }
+
+metrics = [
+    "loss/train",
+    "loss/validation/dataloader_idx_0",
+    "loss/validation/dataloader_idx_1",
+    "MSE/validation/dataloader_idx_0",
+    "MSE/validation/dataloader_idx_1",
+    "SMAPE/validation/dataloader_idx_0",
+    "SMAPE/validation/dataloader_idx_1",
+]
 
 
 def apply_config(
     config: dict[str, typing.Any], cli_config: dict[str, typing.Any]
-) -> dict:
+) -> dict[str, typing.Any]:
+    """
+    Apply the hyperparameters to the config using the PARAM2KEY mapping.
+
+    Parameters
+    ----------
+    config : dict
+        The hyperparameters to apply.
+    cli_config : dict
+        The config to apply the hyperparameters to, which is a config for
+        LightningCLI.
+
+    Returns
+    -------
+    dict
+        The new config with the hyperparameters applied.
+    """
     new_config = cli_config.copy()
 
     def apply_key(
         config: dict[str, typing.Any], key: list[str], value: typing.Any
     ) -> None:
+        """Recursively apply the key to the config."""
         if len(key) == 1:
             config[key[0]] = value
         else:
@@ -71,16 +90,9 @@ def apply_config(
     return new_config
 
 
-def make_tune_callback() -> dict[str, typing.Any]:
-    metrics = [
-        "loss/train",
-        "loss/validation/dataloader_idx_0",
-        "loss/validation/dataloader_idx_1",
-        "MSE/validation/dataloader_idx_0",
-        "MSE/validation/dataloader_idx_1",
-        "SMAPE/validation/dataloader_idx_0",
-        "SMAPE/validation/dataloader_idx_1",
-    ]
+def make_tune_callback(metrics: list[str] | None = None) -> dict[str, typing.Any]:
+    """Defines the TuneReportCallback."""
+    metrics = metrics or []
 
     return {
         "class_path": "transformertf.tune.TuneReportCallback",
@@ -100,7 +112,7 @@ def train_fn(
 
     if "callbacks" not in new_config["trainer"]:
         new_config["trainer"]["callbacks"] = []
-    tune_callback = make_tune_callback()
+    tune_callback = make_tune_callback(metrics)
     new_config["trainer"]["callbacks"].append(tune_callback)
 
     cli = LightningCLI(args=new_config, run=False)
@@ -130,18 +142,9 @@ def main() -> None:
     with open(CONFIG_PATH, encoding="locale") as f:
         cli_config = yaml.safe_load(f)
     cli_config.pop("ckpt_path")
-
     reporter = ray.tune.CLIReporter(
         parameter_columns=list(GRID.keys()),
-        metric_columns=[
-            "loss/train",
-            "loss/validation/dataloader_idx_0",
-            "loss/validation/dataloader_idx_1",
-            "MSE/validation/dataloader_idx_0",
-            "MSE/validation/dataloader_idx_1",
-            "SMAPE/validation/dataloader_idx_0",
-            "SMAPE/validation/dataloader_idx_1",
-        ],
+        metric_columns=metrics,
     )
     scheduler = ray.tune.schedulers.PopulationBasedTraining(
         time_attr="training_iteration",
@@ -178,7 +181,7 @@ def main() -> None:
     )
 
     results_grid = tuner.fit()
-    results_grid.get_dataframe().to_parquet(HERE / "pbt_results.parquet")
+    results_grid.get_dataframe().to_parquet(HERE / "pbt_results/results.parquet")
 
 
 if __name__ == "__main__":
