b17fc7268cc35b52f589be431d1c6e4bc46cb51d
Anton Lu <anton.lu@cern.ch>
2024-07-10 16:52:58 +0200

    fix log pytest

--- transformertf/data/transform/_log.py
+++ transformertf/data/transform/_log.py
@@ -11,6 +11,7 @@ import numpy as np
 import torch
 
 from ._base import BaseTransform, TransformType
+from ._utils import _as_torch
 
 
 class LogTransform(BaseTransform):
@@ -31,12 +32,12 @@ class LogTransform(BaseTransform):
     def transform(
         self, x: torch.Tensor | np.ndarray, y: torch.Tensor | np.ndarray | None = None
     ) -> torch.Tensor:
-        return torch.log(x)
+        return torch.log(_as_torch(x))
 
     def inverse_transform(
         self, x: torch.Tensor | np.ndarray, y: torch.Tensor | np.ndarray | None = None
     ) -> torch.Tensor:
-        return torch.exp(x)
+        return torch.exp(_as_torch(x))
 
     def __sklearn_is_fitted__(self) -> bool:  # noqa: PLW3201
         return True
