ac3e5ae42ead4a1009c2f501928387d5d0183d58
Anton Lu <anton.lu@cern.ch>
2024-07-30 14:52:48 +0200

    refactor transform tests to own submodule

--- tests/data/transform/test_transforms.py
+++ tests/data/transform/test_transforms.py
@@ -7,8 +7,6 @@ from sklearn.utils.validation import check_is_fitted
 
 from transformertf.data import FixedPolynomialTransform, PolynomialTransform
 
-from ..conftest import CURRENT, FIELD
-
 
 @pytest.fixture()
 def x() -> torch.Tensor:
@@ -110,38 +108,50 @@ def test_polynomial_transform_three_degree_derivative() -> None:
     assert torch.eq(derivative.weights, torch.tensor([6.0, 9.0])).all()
 
 
-def test_polynomial_transform_fit(df: pd.DataFrame) -> None:
+def test_polynomial_transform_fit(
+    df: pd.DataFrame, current_key: str, field_key: str
+) -> None:
     transform = PolynomialTransform(degree=2, num_iterations=10)
 
-    transform.fit(df[CURRENT].values, df[FIELD].values)
+    transform.fit(df[current_key].to_numpy(), df[field_key].to_numpy())
 
     check_is_fitted(transform)
 
 
-def test_polynomial_transform_transform(df: pd.DataFrame) -> None:
+def test_polynomial_transform_transform(
+    df: pd.DataFrame, current_key: str, field_key: str
+) -> None:
     transform = PolynomialTransform(degree=2, num_iterations=10)
 
-    transform.fit(df[CURRENT].values, df[FIELD].values)
+    transform.fit(df[current_key].to_numpy(), df[field_key].to_numpy())
 
     check_is_fitted(transform)
 
-    transformed = transform.transform(df[CURRENT].values, df[FIELD].values)
+    transformed = transform.transform(
+        df[current_key].to_numpy(), df[field_key].to_numpy()
+    )
 
     assert transformed.shape == (len(df),)
 
 
-def test_polynomial_transform_inverse_transform(df: pd.DataFrame) -> None:
+def test_polynomial_transform_inverse_transform(
+    df: pd.DataFrame, current_key: str, field_key: str
+) -> None:
     transform = PolynomialTransform(degree=2, num_iterations=10)
 
-    transform.fit(df[CURRENT].values, df[FIELD].values)
+    transform.fit(df[current_key].to_numpy(), df[field_key].to_numpy())
 
     check_is_fitted(transform)
 
-    transformed = transform.transform(df[CURRENT].values, df[FIELD].values)
+    transformed = transform.transform(
+        df[current_key].to_numpy(), df[field_key].to_numpy()
+    )
 
     assert transformed.shape == (len(df),)
 
-    inverse_transformed = transform.inverse_transform(df[CURRENT].values, transformed)
+    inverse_transformed = transform.inverse_transform(
+        df[current_key].to_numpy(), transformed
+    )
 
     assert inverse_transformed.shape == (len(df),)
 
