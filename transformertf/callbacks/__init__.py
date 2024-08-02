ae5a53eef4b4cbeb57a6424ad31d1945efcc89b0
Anton Lu <anton.lu@cern.ch>
2024-08-02 22:53:30 +0200

    merge

--- transformertf/callbacks/__init__.py
+++ transformertf/callbacks/__init__.py
@@ -1,16 +1,18 @@
 from .._mod_replace import replace_modname
+from ._plot_hysteresis import PlotHysteresisCallback
 from ._set_optimizer_lr import SetOptimizerLRCallback
 from ._set_optimizer_params import SetOptimizerParamsCallback
 
 replace_modname(SetOptimizerParamsCallback, __name__)
+replace_modname(PlotHysteresisCallback, __name__)
 replace_modname(SetOptimizerLRCallback, __name__)
 
 
 del replace_modname
 
 
-__all__ = ["SetOptimizerParamsCallback"]
 __all__ = [
+    "PlotHysteresisCallback",
     "SetOptimizerLRCallback",
     "SetOptimizerParamsCallback",
 ]
