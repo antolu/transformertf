387357bde1e66303697c185d019a7309ac626d2f
Anton Lu <anton.lu@cern.ch>
2024-08-02 15:36:45 +0200

    add SetOptimizerLRCallback

--- transformertf/callbacks/__init__.py
+++ transformertf/callbacks/__init__.py
@@ -1,10 +1,16 @@
 from .._mod_replace import replace_modname
+from ._set_optimizer_lr import SetOptimizerLRCallback
 from ._set_optimizer_params import SetOptimizerParamsCallback
 
 replace_modname(SetOptimizerParamsCallback, __name__)
+replace_modname(SetOptimizerLRCallback, __name__)
 
 
 del replace_modname
 
 
 __all__ = ["SetOptimizerParamsCallback"]
+__all__ = [
+    "SetOptimizerLRCallback",
+    "SetOptimizerParamsCallback",
+]
---
+++ transformertf/callbacks/_set_optimizer_lr.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import logging
+import os
+import pathlib
+import typing
+from typing import Any
+
+import lightning as L
+
+from ..models import LightningModuleBase
+
+log = logging.getLogger(__name__)
+
+
+class SetOptimizerLRCallback(L.pytorch.callbacks.Callback):
+    """Sets learning rate based on contents of a file"""
+
+    def __init__(
+        self,
+        lr_file: str | os.PathLike = "/tmp/lr.txt",
+        on: typing.Literal["epoch", "step"] = "epoch",
+    ):
+        super().__init__()
+        self.lr_file = pathlib.Path(lr_file)
+        self.on = on
+
+    def on_train_epoch_start(
+        self, trainer: L.pytorch.Trainer, pl_module: LightningModuleBase
+    ) -> None:
+        if self.on == "epoch":
+            self.set_lr(trainer)
+
+    def on_train_batch_start(
+        self,
+        trainer: L.pytorch.Trainer,
+        pl_module: LightningModuleBase,
+        batch: typing.Any,
+        batch_idx: int = 0,
+        dataloader_idx: int = 0,
+    ) -> None:
+        if self.on == "step":
+            self.set_lr(trainer)
+
+    def set_lr(self, trainer: L.pytorch.Trainer) -> None:
+        if not self.lr_file.exists():
+            log.debug(f"File {self.lr_file} does not exist.")
+            return
+
+        with open(self.lr_file, encoding="utf-8") as f:
+            try:
+                contents = f.read().strip()
+            except ValueError:
+                log.exception(
+                    f"Could not read LR from file {self.lr_file}, "
+                    f"Contents: {contents}"
+                )
+                return
+
+            try:
+                lr = float(contents)
+            except ValueError:
+                log.exception(
+                    f"Could not convert contents of file {self.lr_file} "
+                    f"to float: {contents}"
+                )
+                return
+
+        log.info(f"Setting LR to {lr}.")
+        for optimizer in trainer.optimizers:
+            for param_group in optimizer.param_groups:
+                param_group["lr"] = lr
+
+        trainer.strategy.barrier()
+
+        if trainer.is_global_zero:
+            log.info(f"LR set to {lr} on all processes, removing {self.lr_file}")
+
+    def state_dict(self) -> dict[str, Any]:
+        return {"lr_file": str(self.lr_file)}
+
+    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+        if "lr_file" in state_dict:
+            self.lr_file = pathlib.Path(state_dict["lr_file"])
+
+        state_dict.pop("lr_file", None)
