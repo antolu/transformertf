40f5d55706c4f08a1157fc7d7857a231d7f56f9b
Anton Lu <anton.lu@cern.ch>
2024-08-05 20:13:53 +0200

    guard against optional arguments being none

--- transformertf/main.py
+++ transformertf/main.py
@@ -105,7 +105,7 @@ class LightningCLI(lightning.pytorch.cli.LightningCLI):
             "trainer.logger": jsonargparse.lazy_instance(
                 lightning.pytorch.loggers.TensorBoardLogger,
                 save_dir="logs",
-                name=None,
+                name="",
             ),
         })
 
@@ -119,24 +119,35 @@ class LightningCLI(lightning.pytorch.cli.LightningCLI):
 
     def before_fit(self) -> None:
         # hijack model checkpoint callbacks to save to checkpoint_dir/version_{version}
-        if hasattr(self.config, "fit") and hasattr(self.config.fit, "experiment_name"):
+        if (
+            hasattr(self.config, "fit")
+            and hasattr(self.config.fit, "experiment_name")
+            and self.config.fit.experiment_name
+        ):
             logger_name = self.config.fit.experiment_name
             self.trainer.logger._name = logger_name  # noqa: SLF001
         else:
             logger_name = ""
-        version = self.trainer.logger.version
+        try:
+            version = self.trainer.logger.version
+        except TypeError:
+            version = 0
         version_str = f"version_{version}"
 
         for callback in self.trainer.callbacks:
             if isinstance(callback, lightning.pytorch.callbacks.ModelCheckpoint):
                 if logger_name:
-                    dirpath = os.path.join(callback.dirpath, version_str)
-                else:
                     dirpath = os.path.join(callback.dirpath, logger_name, version_str)
+                else:
+                    dirpath = os.path.join(callback.dirpath, version_str)
                 callback.dirpath = dirpath
 
         # load checkpoint for transfer learning
-        if hasattr(self.config, "fit") and hasattr(self.config.fit, "transfer_ckpt"):
+        if (
+            hasattr(self.config, "fit")
+            and hasattr(self.config.fit, "transfer_ckpt")
+            and self.config.fit.transfer_ckpt is not None
+        ):
             transfer_ckpt = os.fspath(
                 pathlib.Path(self.config.fit.transfer_ckpt).expanduser()
             )
