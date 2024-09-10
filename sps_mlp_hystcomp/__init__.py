daee7d5dc6687cc2a021deaeebd3098170a75d24
Anton Lu <anton.lu@cern.ch>
2024-09-10 17:43:00 +0200

    move predictors to package root

--- sps_mlp_hystcomp/__init__.py
+++ sps_mlp_hystcomp/__init__.py
@@ -1,1 +1,13 @@
-from ._version import version as __version__  # noqa
+from _version import version as __version__  # noqa
+
+from ._base_predictor import Predictor
+from ._mod_replace import replace_modname
+from ._pete_predictor import PETEPredictor
+from ._tft_predictor import TFTPredictor
+
+for _mod in (Predictor, PETEPredictor, TFTPredictor):
+    replace_modname(_mod, __name__)
+
+
+del replace_modname
+del _mod
