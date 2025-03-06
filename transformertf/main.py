322b1caeb83ad11319c1670db6569cf01a742b01
Anton Lu <anton.lu@cern.ch>
2025-03-06 17:26:10 +0100

    change log sys/argv to source_code/argv

--- transformertf/main.py
+++ transformertf/main.py
@@ -165,7 +165,7 @@ class LightningCLI(lightning.pytorch.cli.LightningCLI):
                     )
 
             # log the command used to launch training to neptune
-            self.trainer.logger.experiment["sys/argv"] = " ".join(sys.argv)
+            self.trainer.logger.experiment["source_code/argv"] = " ".join(sys.argv)
 
             self.trainer.logger.experiment.sync()
 
