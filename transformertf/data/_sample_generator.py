correct computation of decoder_lengths for EncoderDecoderPredictDataset

transformertf/data/_sample_generator.py
@@ -468,15 +468,35 @@ class TransformerPredictionSampleGenerator(SampleGenerator[EncoderDecoderSample[
             tgt = tgt.reset_index(drop=True)
             decoder_mask = decoder_mask.reset_index(drop=True)
 
+        encoder_lengths: T
+        if isinstance(src, pd.DataFrame):
+            encoder_lengths = pd.DataFrame({"encoder_lengths": [1.0]})  # type: ignore[assignment]
+        elif isinstance(src, torch.Tensor):
+            encoder_lengths = torch.tensor([1.0])  # type: ignore[assignment]
+        else:
+            encoder_lengths = np.array([1.0])  # type: ignore[assignment]
+
+        decoder_lengths: T
+        if isinstance(tgt, pd.DataFrame):
+            decoder_lengths = pd.DataFrame({  # type: ignore[assignment]
+                "decoder_lengths": [self._prediction_length - numel_pad]
+            })
+        elif isinstance(tgt, torch.Tensor):
+            decoder_lengths = torch.tensor(  # type: ignore[assignment]
+                [self._prediction_length - numel_pad], dtype=torch.float32
+            )
+        else:
+            decoder_lengths = np.array([self._prediction_length - numel_pad])  # type: ignore[assignment]
+
         return typing.cast(
             EncoderDecoderSample[T],
             {
                 "encoder_input": src,
                 "encoder_mask": encoder_mask,
-                "encoder_lengths": zeros_like(src) + self._context_length,
+                "encoder_lengths": encoder_lengths,
                 "decoder_input": tgt,
                 "decoder_mask": decoder_mask,
-                "decoder_lengths": zeros_like(tgt) + self._prediction_length,
+                "decoder_lengths": decoder_lengths,
             },
         )
 

transformertf/data/dataset/_encoder_decoder_predict.py
@@ -255,9 +255,10 @@ class EncoderDecoderPredictDataset(
         sample_torch["encoder_lengths"] = torch.ones(
             (1,), dtype=self._dtype, device=sample_torch["encoder_input"].device
         )
-        sample_torch["decoder_lengths"] = torch.ones(
-            (1,), dtype=self._dtype, device=sample_torch["encoder_input"].device
-        )
+
+        # decoder lengths must be computed from actual number of future time steps
+        sample_torch["decoder_lengths"] /= self._target_length
+        sample_torch["decoder_lengths"] = sample_torch["decoder_lengths"].flatten()
 
         return sample_torch
 